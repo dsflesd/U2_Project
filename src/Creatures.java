d3b3852c59ad3f45a892626c918cddeb69d328a8
dsflesd <andyz119@nycstudents.net>
2025-11-18 08:41:29 -0500

    Add files via upload

--- src/Creatures.java
+++ src/Creatures.java
@@ -1,7 +1,18 @@
 public class Creatures {
-    private int health;
-    private int strength;
-    private int defence;
+    private int healthStat;
+    private int strengthStat;
+    private int defenceStat;
+    private String name;
+
+    public Creatures(String name, int healthStat, int strengthStat, int defenceStat){
+        this.healthStat=healthStat;
+        this.strengthStat=strengthStat;
+        this.defenceStat=defenceStat;
+        this.name=name;
+    }
+    public void atributeCalculator (String prioritizedAttribute){
+
+    }
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
+import java.util.Objects;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Create your creature!");
-        System.out.println("What is the name of your creature?");
+        System.out.println("Create a new creature?");
+        String ans =scan.nextLine();
+        if (ans.equals("yes")||ans.equals("Yes")){
+            System.out.println("What is the name of your creature?");
+            String name=scan.nextLine();
+            Creatures rawr = new Creatures(name, 0, 0, 0);
+            System.out.println("You have "+(int)((Math.random()*26)+75)+" skill points");
+
+            System.out.println("Which attribute would you like to prioritize?");
+            String prioritezedAtribute=scan.nextLine();
+            rawr.atributeCalculator(prioritezedAtribute);
+        }
+        else {
+            System.out.println("Maybe next time.");
+        }
+
+
         scan.close();
     }
 }
